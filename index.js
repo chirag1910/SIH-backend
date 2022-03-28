dac6c2856bb77748966dc7ecc2370f609e76509d
Chirag <chiraggoyal1910@gmail.com>
2022-03-28 16:50:41 +0530

    Added scheme routes in index.js

--- index.js
+++ index.js
@@ -10,8 +10,10 @@ app.use(cookieParser());
 app.use(cors({ credentials: true, origin: true }));
 
 const userRoutes = require("./Routes/user");
+const schemeRoutes = require("./Routes/scheme");
 
 app.use("/user", userRoutes);
+app.use("/scheme", schemeRoutes);
 
 const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => {
